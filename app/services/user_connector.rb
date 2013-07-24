798682ba3405e0d67d1505a72fb58472ac25c319
Shay Davidson <shay.h.davidson@gmail.com>
2013-07-24 14:58:45 +0300

    removed port thingy

--- app/services/user_connector.rb
+++ app/services/user_connector.rb
@@ -4,37 +4,35 @@ class UserConnector
 
   def self.connect_from_omniauth(auth_hash)
     case auth_hash.provider.to_sym
-      when :developer then UserConnector::OmniAuth::Developer.connect(auth_hash)
-      when :github    then UserConnector::OmniAuth::GitHub.connect(auth_hash)
+      when :developer then UserConnector::Developer.connect(auth_hash)
+      when :github    then UserConnector::GitHub.connect(auth_hash)
       else raise UnsupportedProviderError, auth_hash.provider
     end
   end
 
-  module OmniAuth
-    class Developer
-      def self.connect(auth_hash)
-        User.where(:uid => auth_hash.uid).first_or_create(:email => auth_hash.info.email,
-                                                          :name  => auth_hash.info.name)
-      end
+  class Developer
+    def self.connect(auth_hash)
+      User.where(:uid => auth_hash.uid).first_or_create(:email => auth_hash.info.email,
+                                                        :name  => auth_hash.info.name)
+    end
+  end
+
+  class GitHub
+
+    class UnauthorizedGithubUser < StandardError; end
+
+    def self.connect(auth_hash)
+      raise UnauthorizedGithubUser unless authorized?(auth_hash.uid)
+
+      User.where(:uid => auth_hash.uid).first_or_create(:email    => auth_hash.info.email,
+                                                        :name     => auth_hash.info.name,
+                                                        :nickname => auth_hash.info.nickname,
+                                                        :image    => auth_hash.info.image)
     end
 
-    class GitHub
-
-      class UnauthorizedGithubUser < StandardError; end
-
-      def self.connect(auth_hash)
-        raise UnauthorizedGithubUser unless authorized?(auth_hash.uid)
-
-        User.where(:uid => auth_hash.uid).first_or_create(:email    => auth_hash.info.email,
-                                                          :name     => auth_hash.info.name,
-                                                          :nickname => auth_hash.info.nickname,
-                                                          :image    => auth_hash.info.image)
-      end
-
-      def self.authorized?(uid)
-        members = Octokit.organization_members(ENV['AUTHORIZED_GITHUB_ORGANIZATION'])
-        members.find { |member| member.id.to_s == uid }
-      end
+    def self.authorized?(uid)
+      members = Octokit.organization_members(ENV['AUTHORIZED_GITHUB_ORGANIZATION'])
+      members.find { |member| member.id.to_s == uid }
     end
   end
 

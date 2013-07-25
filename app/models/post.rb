9b2777283783577eea9d2e002ecacff5cf1a6716
Ido Feins <ifeins@gmail.com>
2013-07-25 11:30:11 +0300

    changed default story from none to nothing since none is a reserved scope for activemodel in rails 4.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  enum :story, [:none, :to_wip, :to_done, :to_dead]
+  enum :story, [:nothing, :to_wip, :to_done, :to_dead]
 
 end

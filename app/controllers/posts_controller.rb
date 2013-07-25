073540ec8ab31ea320535b8c4787c951b331b5b4
Ido Feins <ifeins@gmail.com>
2013-07-25 14:25:34 +0300

    some cleanup.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,20 @@
 class PostsController < ApplicationController
 
+  include ProjectSupport
+
+  before_filter :sign_in_required
+  before_filter :has_project
+
   def create
+    current_project.posts.create(post_params)
+    redirect_to current_project
+  end
+
+  private
+
+  def post_params
+    params[:user] = current_user
+    params.require(:post).permit(:text, :user)
   end
 
 end

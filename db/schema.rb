df3d20f9ad7ce0a126e7f2fc06cda42b22b867a2
Ido Feins <ifeins@gmail.com>
2013-07-25 11:30:11 +0300

    changed default story from none to nothing since none is a reserved scope for activemodel in rails 4.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130724121924) do
+ActiveRecord::Schema.define(version: 20130725082646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20130724121924) do
     t.integer "project_id"
     t.integer "user_id"
     t.string  "text",       limit: 1024
-    t.string  "story",                   default: "none"
+    t.string  "story",                   default: "nothing"
     t.boolean "update",                  default: false
   end
 

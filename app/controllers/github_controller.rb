cfe970864afafcce0876a46945ad89c65779c2d8
Adam Farhi <afarhi@ebay.com>
2013-07-26 11:44:39 +0300

    update hook controller

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,21 +1,11 @@
+require 'services/github_service_hook'
+
 class GithubController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
 
   def post_receive_hook
-
-    payload = params[:payload]
-
-    return Log.error('Github Post-Receive Hook received empty payload') if payload.blank?
-
-
-    payload = JSON.parse(payload)
-
-    # check if payload.ref == "refs/heads/master" ??
-
-    # find project by payload.repository.url
-    # project.update_last_commit  payload.head_commit.timestamp
-
+    GithubServiceHook.new(params[:payload]).process_payload
   end
 
 end

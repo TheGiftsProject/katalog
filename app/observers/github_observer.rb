49195509450cde69ea4826bf42f53b2755e80d02
Adam Farhi <afarhi@ebay.com>
2013-07-28 13:54:51 +0300

    added after_update

--- app/observers/github_observer.rb
+++ app/observers/github_observer.rb
@@ -5,11 +5,21 @@ class GithubObserver < ActiveRecord::Observer
   observe :project
 
   def after_create(project)
-    project.subscribe_to_service_hook
+    project.subscribe_to_service_hook if should_use_service_hook?(project)
+  end
+
+  def after_update(project)
+    project.subscribe_to_service_hook if should_use_service_hook? and project.repo_url_was.blank?
   end
 
   def after_destory(project)
-    project.unsubscribe_to_service_hook
+    project.unsubscribe_to_service_hook if should_use_service_hook?(project)
+  end
+
+  private
+
+  def should_use_service_hook?(project)
+    project.repo_url.present?
   end
 
 end

daee4f2eefc3dc5c9b467fcafa3fa9f3f3d29ebf
Adam Farhi <afarhi@ebay.com>
2013-07-30 23:39:53 +0300

    github support specs

---
+++ spec/controllers/concerns/github_support_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'GithubSupport', :controller do
+
+  controller do
+    include GithubSupport
+
+    def index
+      @hook_callback_url = GithubGrabber.hook_callback_url
+      render :nothing => true
+    end
+  end
+
+  let(:current_project) { create(:project) }
+  let(:hook_callback_url) { project_post_receive_hook_url(current_project) }
+
+  describe :set_github_grabber_host do
+
+    before do
+      allow(controller).to receive(:current_project).and_return(current_project)
+    end
+
+    it "sets Github's service hook callback url before any action" do
+      get :index
+      expect(assigns[:hook_callback_url]).to eq hook_callback_url
+    end
+
+  end
+
+end

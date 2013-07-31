fb71af0302d8c38e5abf54ee93610761d0b44f4c
yon <Lighthawky@gmail.com>
2013-07-31 11:54:33 +0300

    trying to solve issue with emoji

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Katalog
     # config.i18n.default_locale = :de
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.compile = true
 
     config.exceptions_app = self.routes
   end

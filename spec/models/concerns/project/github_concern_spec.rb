e6b646150312b4647ddff28257710b2ee0140f25
Adam Farhi <afarhi@ebay.com>
2013-08-01 16:02:13 +0300

    includes_contributors? specs

--- spec/models/concerns/project/github_concern_spec.rb
+++ spec/models/concerns/project/github_concern_spec.rb
@@ -2,10 +2,29 @@ require 'spec_helper'
 
 describe Project::GithubConcern do
 
-  let(:user) { create(:user, :isc_ci) }
+  describe :includes_contributors? do
 
-  let(:github_grabber) { subject.send(:github_grabber) }
+    let(:users) { create_list(:user, 2) }
+    let(:existing_user_emails) { users.map(&:email) }
+    let(:new_contributor_emails) { ['new_contributor1@email.com','new_contributor2@email.com'] }
+    let(:project) { create(:project, users: users) }
+    subject { project.includes_contributors?(given_user_emails) }
 
-  subject { create(:project, :with_repo, users: [user]) }
+    context "when given emails for users who are contributors in the project" do
+      let(:given_user_emails) { existing_user_emails }
+      it { should be_true }
+    end
+
+    context "when given emails for users who aren't contributors in the project" do
+      let(:given_user_emails) { new_contributor_emails }
+      it { should be_false }
+    end
+
+    context "when given emails for users who some are and some aren't contributors in the project" do
+      let(:given_user_emails) { new_contributor_emails + existing_user_emails }
+      it { should be_false }
+    end
+
+  end
 
 end

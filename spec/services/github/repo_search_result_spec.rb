8466c3883f04a4fca56df57f2f807267ef84832c
Adam Farhi <afarhi@ebay.com>
2013-08-02 16:04:31 +0300

    result spec update

--- spec/services/github/repo_search_result_spec.rb
+++ spec/services/github/repo_search_result_spec.rb
@@ -2,14 +2,18 @@ require 'spec_helper'
 
 describe Github::RepoSeachResult do
 
+  let(:repo_id) { '123456' }
+  let(:repo_url) { 'https://github.com/iic-ninjas/MyAwesomeKataRepo' }
   let(:name) { 'repository_name' }
   let(:description) { 'repository_description' }
   let(:language) { 'repository_language' }
 
   subject { Github::RepoSeachResult.new(raw_json) }
 
-  its(:name) { should eq repository_name }
-  its(:description) { should eq repository_description }
-  its(:language) { should eq repository_language }
+  its(:repo_id) { should eq repo_id }
+  its(:repo_url) { should eq repo_url }
+  its(:name) { should eq name }
+  its(:description) { should eq description }
+  its(:language) { should eq language }
 
 end

b42f044198779f42b94d2f18144b03682d4988ee
Adam Farhi <afarhi@ebay.com>
2013-08-05 00:08:51 +0300

    search result ignores unneeded props

--- app/services/github/repo_search_result.rb
+++ app/services/github/repo_search_result.rb
@@ -2,10 +2,22 @@ require 'hashie/trash'
 
 class RepoSearchResult < Hashie::Trash
 
+  def initialize(repo_data)
+    super( self.class.keep_only_defined_properties(repo_data) )
+  end
+
   property :repo_id, :from => :id
   property :repo_url, :from => :html_url
   property :name, :from => :full_name
   property :description
   property :language
+
+  private
+
+  def self.keep_only_defined_properties(raw_data)
+    raw_data = raw_data.to_hash
+    selected_properties = self.class.properties + self.class.translations
+    filtered_data = raw_data.keep_if { |k,v| selected_properties.include?(k) }
+  end
 
 end

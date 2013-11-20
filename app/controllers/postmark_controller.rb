78b63aef1343293a7298e1e6adf1b4bed37bc8fe
Itay Adler <itayadler@gmail.com>
2013-11-20 20:34:24 +0200

    fixes

--- app/controllers/postmark_controller.rb
+++ app/controllers/postmark_controller.rb
@@ -1,8 +1,12 @@
 require 'create_project_from_email'
 
-class PostmarkController
+class PostmarkController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token
+
   def update
     email = Postmark::Mitt.new(request.body.read)
     CreateProjectFromEmail.create(email)
+    render :nothing => true
   end
 end

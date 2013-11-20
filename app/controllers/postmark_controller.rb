742a56b8e3d96671a5de2640bd655a901b048c20
Itay Adler <itayadler@gmail.com>
2013-11-20 21:35:42 +0200

    test

--- app/controllers/postmark_controller.rb
+++ app/controllers/postmark_controller.rb
@@ -1,12 +1,9 @@
 require 'create_project_from_email'
 
-class PostmarkController < ApplicationController
-
-  skip_before_filter :verify_authenticity_token
+class PostmarkController < ActionController::Base
 
   def update
     Rails.logger.info("Incoming post from postmark")
-    Rails.logger.info(request.body.read)
     email = Postmark::Mitt.new(request.body.read)
     CreateProjectFromEmail.create(email)
     render :nothing => true

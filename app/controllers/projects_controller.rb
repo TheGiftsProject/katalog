5e09dbd230dcc19d37f33b646fe85fec01bb8c60
Shay Davidson <shay.h.davidson@gmail.com>
2014-07-20 09:53:46 +0300

    Clouds

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -123,7 +123,7 @@ class ProjectsController < ApplicationController
   end
 
   def set_projects_for_scope(scope)
-    @projects = scope.latest_first.paginate(:page => params[:page]) 
+    @projects = scope.latest_first.paginate(:page => params[:page])
   end
 
   def random_idea

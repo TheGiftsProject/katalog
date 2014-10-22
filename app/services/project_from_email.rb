8267f191ca0bbd1ae4b0e8914693916ef6b5a736
Shay Davidson <shay.h.davidson@gmail.com>
2014-10-22 14:41:30 +0300

    Fixed postmark

--- app/services/project_from_email.rb
+++ app/services/project_from_email.rb
@@ -1,12 +1,12 @@
 class ProjectFromEmail
   def self.create(email_data)
-    user = User.find_by(email: email_data.from_email)
+    user = User.find_by(email: email_data.from)
     if user
-      return nil if user.default_organization
+      return nil if user.default_organization.nil?
 
       title = email_data.subject
       subtitle = email_data.text_body.split("\n").first
-      project = Project.new(title: title, subtitle: subtitle, ideator: user, organization_id: user.default_organization)
+      project = Project.new(title: title, subtitle: subtitle, ideator: user, organization_id: user.default_organization.id)
       user.projects << project
       user.save!
       project

b3447cba2f60c281c80504e817248d4d38ee1651
Shay Davidson <shay.h.davidson@gmail.com>
2014-11-10 11:23:19 +0200

    Fixed katalog with POW in development

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.full_host = "http://katalog.dev" if Rails.env.development?
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end

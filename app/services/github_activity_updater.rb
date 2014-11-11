7864908ae6543328880e04c0db5d4b6155fbc0ed
Shay Davidson <shay.h.davidson@gmail.com>
2014-11-11 14:13:20 +0200

    Updated octokit

--- app/services/github_activity_updater.rb
+++ app/services/github_activity_updater.rb
@@ -9,8 +9,13 @@ class GithubActivityUpdater
 
   def update_activity
     relevant_projects.each do |project|
-      branches = branches_of_project(project)
-      # check that user still exist.
+      repo_name = project.github_repo_name
+      branches = branches_of_project(repo_name)
+      branches.each do |branch_name|
+        commits = commits_of_branch(branch_name)
+        mapped_commits = map_commits(commits)
+        # check that user still exist.
+      end
     end
   end
 
@@ -24,8 +29,21 @@ class GithubActivityUpdater
     Project.where('updated_at > ?', TIME_THRESHOLD_FOR_PROJECT_ACTIVITY_UPDATE.ago).have_github_repo
   end
 
-  def branches_of_project(project)
+  def branches_of_project(repo_name)
+    @api.branches(repo_name).map(&:name)
+  end
 
+  def commits_of_branch(branch_name)
+    @api.commits_since(repo_name, TIME_SINCE_LAST_ACTIVITY_UPDATE.ago, branch_name)
+  end
+
+  def map_commits(commits)
+    commits.map do |commit|
+      {
+        :updated_at => commit[:commit][:author][:date],
+        :committer => commit[:committer][:login]
+      }
+    end
   end
 
 end

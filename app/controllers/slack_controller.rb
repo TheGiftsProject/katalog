c45fb106bdcc5dcd15aceb56acbcf1ab442d4982
yon <Lighthawky@gmail.com>
2016-02-28 18:05:53 +0200

    slack

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -12,11 +12,11 @@ class SlackController < ActionController::Base
   def create
     @user = User.joins(:organizations).where(organizations: {slack_token: params[:token]}, slack_id: params[:user_id]).first
     unless @user
-      render inline: "Hey, welcome to Katalog. I don't know who you are please go to <#{root_url}slack> and sign in"
+      render inline: "Hey, welcome to Katalog. I don't know who you are please go to <#{root_url}slack> and sign in."
       return
     end
     project = ProjectFromSlack.new.create_project @user, params[:text]
-    render inline: "Done created project - #{projects_url(project)}"
+    render inline: "Done created your new project -> <#{projects_url(project)}>"
   end
 
 
